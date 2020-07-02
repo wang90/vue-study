43fc25d9d30b7ab787997e5e4072f9631e0522b6
wang90 <393210564@qq.com>
2020-07-02 14:09:22 +0800

    Create vuex.js

---
+++ vuex.js
@@ -0,0 +1,26 @@
+// vuex.js
+
+
+improt {reactive} from "./main";
+
+class  Store{
+    constructor( options= {}) {
+        let { state , mutations} = options;
+        this._vm = reactive(state);
+        this._mutations = mutations;
+    }
+    get state(){
+        return this._vm;
+    }
+    commit (type,payload){
+        const entry = this._mutations[type];
+        if (!entry ) {
+            return; 
+        }
+        entry(this.state,payload);
+    }
+}
+
+
+
+
