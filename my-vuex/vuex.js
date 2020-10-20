1deecc29d9de0a06798bb39acb4c41e67cfd914f
wang90 <393210564@qq.com>
2020-10-20 22:01:09 +0800

    Update vuex.js

--- my-vuex/vuex.js
+++ my-vuex/vuex.js
@@ -1,21 +1,21 @@
 // vuex.js
 improt { reactive } from "./main"; // 利用之前绑定好的reactive
 
-class  Store{
-    constructor( options= {}) {
-        let { state , mutations} = options;
-        this._vm = reactive(state);
+class  Store {
+    constructor( options= {} ) {
+        let { state , mutations } = options;
+        this._vm = reactive( state );
         this._mutations = mutations;
     }
-    get state(){
+    get state() {
         return this._vm;
     }
-    commit (type,payload){
-        const entry = this._mutations[type];
+    commit ( type, payload ) {
+        const entry = this._mutations[ type ];
         if (!entry ) {
             return; 
         }
-        entry(this.state,payload);
+        entry( this.state, payload );
     }
 }
 
